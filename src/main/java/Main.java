82637dd8092995ad487042491e26f31bf5df9a61
Ganesh-Paudel <paudelganesh080@gmail.com>
2026-03-03 17:07:45 -0500

    Added the loop in main, runs and reads the line, till user enters exit or quit.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+
+    static String input;
+    static  boolean isRunning = true;
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello, MY Database!!");
+
+        while(isRunning){
+           sidePrompt();
+           readInput();
+
+           input = input.toLowerCase();
+           if(input.equals("exit") || input.equals("Quit")){
+               exitProgram();
+           } else{
+               System.out.println("What is this command? HUH!!!!!, This is not a valid command.");
+           }
+        }
+    }
+    private static void readInput(){
+       input = scan.nextLine();
+    }
+
+    private static void sidePrompt(){
+        System.out.print(" myDatabase>>> ");
+    }
+
+    private static void exitProgram(){
+       isRunning = false;
     }
 }
